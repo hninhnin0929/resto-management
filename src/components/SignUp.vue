ba486f362a8c8acb047a963b945e59861447b6d8
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-29 18:16:30 +0900

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <img class="logo" src="../assets/res_logo.png">
+        <h1>Sign Up</h1>
+        <div class="register">
+            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Enter Email" />
+            <input type="text" placeholder="Enter Password" />
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.logo{
+    width: 100px
+}
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+.register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
