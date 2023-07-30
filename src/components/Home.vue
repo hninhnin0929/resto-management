fa109c29fa8d865d8071ad93890c9ff369843521
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-30 15:48:01 +0900

    add restaurant list in home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,26 +2,52 @@
     <div>
         <Header />
         <h1>Hello {{ name }}, Welcome to Our Homepage</h1>
+        <table border="1">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Contact</th>
+                <th>Address</th>
+            </tr>
+            <tr v-for="item in restaurant" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.contact }}</td>
+                <td>{{ item.address }}</td>
+            </tr>
+        </table>
     </div>
 </template>
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
 export default {
     name: 'Home',
     data(){
         return {
-            name: ''
+            name: '',
+            restaurant: []
         }
     },
     components: {
         Header
     },
-    mounted(){
+
+    async mounted(){
         let user = localStorage.getItem('user-info');
         this.name = JSON.parse(user).name;    
         if(!user){
             this.$router.push({name: 'SignUp'})
         }
+        let result = await axios.get('http://localhost:3000/restaurant');
+        console.warn(result);
+        this.restaurant = result.data;
     }
 }
 </script>
+<style>
+ td{
+    width: 160px;
+    height: 40px;
+ }
+</style>
